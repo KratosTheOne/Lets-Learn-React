789cf3050d32ef7722db2f221dfd571092b6e73b
Kaustubh <kaustubhsinha2001@gmail.com>
2022-12-24 23:12:48 +0530

    commit

---
+++ Day 1/Code/Using React/App.js
@@ -0,0 +1,27 @@
+const heading = React.createElement(
+    "h1",
+    {
+        id : "title"
+    },
+    "This is heading 1"
+);
+
+const heading2 = React.createElement(
+    "h2",
+    {
+        id : "title"
+    },
+    "This is heading 2"
+);
+
+const container = React.createElement(
+    "div",
+    {
+        id : "container"
+    },
+    [heading, heading2]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
