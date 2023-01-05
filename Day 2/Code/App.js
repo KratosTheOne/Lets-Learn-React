ecc8a4e6de7955c65de8e6d401a34f7025f10db3
Kaustubh <kaustubhsinha2001@gmail.com>
2023-01-06 00:00:16 +0530

    commit

--- Day 2/Code/App.js
+++ Day 2/Code/App.js
@@ -1,9 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ *HMR - Hot Module Replacement - parcel keeps track of file changes via file watcher algorithm and renders the changes in the files
+File watcher algorithm - made with C++
+MINIFY
+BUNDLING
+Cleaning our code
+DEV and production Build
+Super fast building algorithm
+does image optimization also
+Caching while development
+Compresses
+Compatible with older version of browser
+HTTPS in dev
+port Number
+Consistent hashin algorithm
+Zero Configuration
+*/
+
 const heading = React.createElement(
     "h1",
     {
         id : "title"
     },
-    "This is heading 1"
+    "This is heading 1 from parcel"
 );
 
 const heading2 = React.createElement(
